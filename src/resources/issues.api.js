e681ba347f7c473741b22f66c68c530544df0c84
andemerie <imvictoriadrozd@gmail.com>
2017-08-13 20:47:29 +0300

    implement add new issue, search text functionality

--- src/resources/issues.api.js
+++ src/resources/issues.api.js
@@ -1,13 +1,26 @@
 import { displayAll } from './utils';
 
 export function fetchIssues() {
-  const TOKEN = '35e4f74accbceefc27e271ead5d2848cdb3bdbc9';
+  const TOKEN = '2c77a2e49e9d3223936f4e447228cd22443c4bac';
 
   return fetch(`https://api.github.com/repos/andemerie/01_git_task/issues?access_token=${TOKEN}&state=all`)
     .then(response => response.json())
-    .then(data => displayAll(data));
+    .then(data => displayAll(data)
+    );
 }
 
 export function closeIssue(id) {
   return new Promise(resolve => resolve({id}));
+}
+
+export function addNewIssue(data) {
+  return new Promise(resolve => resolve({
+    id: data[data.length - 1].id + 1,
+    title: 'New issue',
+    state: 'open'
+  }));
+}
+
+export function searchText(text) {
+  return new Promise(resolve => resolve({text}));
 }
